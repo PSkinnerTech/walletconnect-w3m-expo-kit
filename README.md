983763cd77565fd3d990da06049a080d467e9463
Patrick Skinner <78289253+PSkinnerTech@users.noreply.github.com>
2023-10-29 22:26:06 -0500

    Added w3m to /app/_layout.tsx and the connect wallet button to two.tsx.

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# walletconnect-w3m-expo-kit
+# WalletConnect's Web3Modal Expo Starter Kit
+
+This Starter Kit is designed for Expo ^49.0.0 with `expo-router`. It provides an easy-to-use foundation for integrating WalletConnect's `web3modal` into your Expo app.
+
+## Features
+
+- Expo ^49.0.0 setup with TypeScript.
+- Routing with `expo-router`.
+- WalletConnect's `web3modal` integration.
+- Pre-configured for mainnet, polygon, and arbitrum chains.
+
+## Prerequisites
+
+- [Node.js](https://nodejs.org/)
+
+## Getting Started
+
+1. **Clone the Repository**:
+
+   ```bash
+   git clone https://github.com/PSkinnerTech/walletconnect-w3m-expo-kit.git
+   cd walletconnect-w3m-expo-kit
+   ```
+
+2. **Install Dependencies**:
+
+   ```bash
+   yarn install
+   ```
+
+3. **Start the Expo App**:
+
+   ```bash
+   npx expo start
+   ```
+
+4. **Run Simulation**:
+   You can run simulation, but to best simulate the WalletConnect experience, you should run the app on a physical device by scanning the QR code with the Expo Go app.
+
+### Update Project Metadata
+
+In the `/app/_layout.tsx` file, you can update the `metadata` object to reflect your project's details:
+
+```typescript
+const metadata = {
+  name: "Your Project Name",
+  description: "Your Project Description",
+  url: "Your Project URL",
+  icons: ["Your Project Icon URL"],
+  redirect: {
+    native: "YourNativeRedirectURL",
+    universal: "YourUniversalRedirectURL",
+  },
+};
+```
+
+### Add More Chains
+
+If you wish to add more chains, update the `chains` array in `/app/_layout.tsx`.
+
+## Troubleshooting
+
+If you encounter any issues:
+
+- Ensure all dependencies are correctly installed.
+- Check the console for any errors and address them.
+- Refer to the official [WalletConnect documentation](https://docs.walletconnect.com/web3modal/react-native/about) for more details on `web3modal`.
+
+## Contributing
+
+We welcome contributions! If you find a bug or have suggestions, please open an issue. If you'd like to contribute code, please fork the repository and submit a pull request.
+
+## License
+
+This project is licensed under the MIT License.
+
+---
+
+Feel free to modify or expand upon this documentation as needed. Let me know if you need any additional sections or details!
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,52 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
-import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
+import {
+  createWeb3Modal,
+  defaultWagmiConfig,
+  Web3Modal,
+} from "@web3modal/wagmi-react-native";
+import { WagmiConfig } from "wagmi";
+import { mainnet, polygon, arbitrum } from "wagmi/chains";
+
+const projectId = "f8d5f6e148c5e3a2cda664423f8bec39";
+
+const metadata = {
+  name: "D_D Starter Kit",
+  description: "D_D Starter Kit created for Expo ^49.0.0 with expo-router.",
+  url: "https://patrickskinner.tech",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+  redirect: {
+    native: "PSkinnerTech://",
+    universal: "https://patrickskinner.tech",
+  },
+};
+
+const chains = [mainnet, polygon, arbitrum];
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+createWeb3Modal({
+  projectId,
+  chains,
+  wagmiConfig,
+});
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -20,7 +54,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -46,11 +80,14 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <WagmiConfig config={wagmiConfig}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        </Stack>
+      </ThemeProvider>
+      <Web3Modal />
+    </WagmiConfig>
   );
 }
